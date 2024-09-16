5f5818d58782c70505300f73e018bf54786eedb3
hasibul1670 <hasibulislam1670@gmail.com>
2024-09-16 18:01:15 +0600

    init :: Bill DTO and Schema Genearated !!

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,16 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import config from './dbConfig/config';
 import { LoggerMiddleware } from './helpers/logger/logger.middleware';
 import { RolesGuard } from './helpers/roleGuard/roles.guard';
 import { AdminModule } from './modules/admin/admin.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { BillModule } from './modules/bill/bill.module';
 import { DatabaseServiceModule } from './modules/database-service/database-service.module';
 import { FeaturesModule } from './modules/features/features.module';
 import { SuperAdminModule } from './modules/super-admin/super-admin.module';
@@ -20,7 +23,12 @@ import { UserModule } from './modules/user/user.module';
       secret: process.env.JWT_ACCESS_SECRET || '454ffdf4df5df13d23',
       signOptions: { expiresIn: '60m' },
     }),
-
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 12,
+      },
+    ]),
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
@@ -41,9 +49,16 @@ import { UserModule } from './modules/user/user.module';
     SuperAdminModule,
     AdminModule,
     FeaturesModule,
+    BillModule,
   ],
   controllers: [AppController],
-  providers: [RolesGuard],
+  providers: [
+    RolesGuard,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [JwtModule],
 })
 export class AppModule implements NestModule {
--- server/src/helpers/validatorMessage/validator-messages.ts
+++ server/src/helpers/validatorMessage/validator-messages.ts
@@ -1,5 +1,6 @@
 export const VALIDATION_MESSAGES = {
   IS_STRING: ' must be a string',
+  IS_NUMBER: ' must be a Number',
   IS_REQUIRED: ' is required',
   IS_BOOLEAN: ' must be a boolean value',
   IS_EMAIL: ' must be an Email',
